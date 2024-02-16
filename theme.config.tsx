b70a912316e54f754b70afed6d9be6ef93892844
Evgeny K <3193631@gmail.com>
2024-02-16 03:14:05 +0100

    feat: place some actual content

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>DI</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/ryodeushii/dev-intro',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.com#',
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Dev Intro',
   },
 }
 
